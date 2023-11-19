d0ee33e43d327adf91f13a8cfcd8466f34a006af
josiel-eway <josiel.jcc@hotmail.com>
2023-11-19 16:28:21 -0300

    fix: input importation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,6 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import Input from "../components/input/input";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
